c0955f19d3aa5450fcf67d232c977febaf820557
Filip <ficatejic@gmai.com>
2024-03-13 20:52:07 +0100

    added connection to database

---
+++ backend/config/db.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const connectToDB = async () => {
+    try {
+        const con = await mongoose.connect(process.env.MONGO_URI, {})
+        console.log(`Connected to MongoDB: ${con.connection.host}`);
+    } catch (error) {
+        console.log(`Error: ${error.message}`);
+    }
+}
+
+module.exports = connectToDB;
--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,12 @@
 const express = require("express");
+const dotenv = require("dotenv");
+const connectToDB = require("./config/db");
+
 const app = express();
 const port = 5000;
+
+dotenv.config();
+connectToDB();
 
 app.get('/', (req, res) => {
     res.send("Hello World");
