966f15b40faa87c9dd6c1e9a5f1d9ce7e00c5089
Filip <ficatejic@gmai.com>
2024-05-13 21:42:34 +0200

    commit

--- frontend/src/socket.ts
+++ frontend/src/socket.ts
@@ -1,5 +1,5 @@
 import { io } from "socket.io-client";
 
-const URL = "http://localhost:5000";
+const URL = "https://jive-backend.onrender.com";
 
 export const socket = io(URL, { autoConnect: false });
